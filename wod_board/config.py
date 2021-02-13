a1acfe22ad83804138bba6fade364b84b69d4137
Guillaume <7831777+GuillaumeOj@users.noreply.github.com>
2021-02-13 14:44:39 +0100

    chore: sort config's variables by alphabetical order

--- wod_board/config.py
+++ wod_board/config.py
@@ -7,8 +7,8 @@ from dotenv import load_dotenv
 
 load_dotenv(find_dotenv())
 
-SECRET_KEY: str = os.getenv("SECRET_KEY", "foo_key_for_testing_purpose")
-HASH_SCHEMES: typing.List[str] = ["bcrypt"]
 DATABASE_URL: str = os.getenv(
     "DATABASE_URL", "postgresql://wod_board:wod_board@localhost/wod_board:5432"
 )
+HASH_SCHEMES: typing.List[str] = ["bcrypt"]
+SECRET_KEY: str = os.getenv("SECRET_KEY", "foo_key_for_testing_purpose")
