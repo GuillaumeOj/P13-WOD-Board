f3bd289d7f8a22e522a18a477e237b84879a5d3f
Guillaume <7831777+GuillaumeOj@users.noreply.github.com>
2021-02-13 18:11:19 +0100

    feat(web): add a password context and OAuth2's schema

--- wod_board/web.py
+++ wod_board/web.py
@@ -1,8 +1,15 @@
 from fastapi import FastAPI
+from fastapi.security import OAuth2PasswordBearer
+from passlib.context import CryptContext
 
+from wod_board import config
 from wod_board.routers import user
 
 
 app = FastAPI()
 
 app.include_router(user.router)
+
+oauth2_schema = OAuth2PasswordBearer(tokenUrl=config.TOKEN_URL)
+
+password_ctxt = CryptContext(schemes=config.HASH_SCHEMES, deprecated="auto")
