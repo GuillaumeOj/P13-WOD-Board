c9eb6bbd9d215690e6d243717fd4d30ed7bccb71
Guillaume <7831777+GuillaumeOj@users.noreply.github.com>
2021-02-25 19:15:57 +0100

    refactor(config): add OAUTH2_SCHEMA and PASSWORD_CTXT
    Move OAUTH2_SCHEMA and PASSWORD_CTXT from 'wod_board.web'
    to 'wod_board.config' to avoid cyclic imports.

--- wod_board/config.py
+++ wod_board/config.py
@@ -3,6 +3,8 @@ import typing
 
 from dotenv import find_dotenv
 from dotenv import load_dotenv
+from fastapi.security import OAuth2PasswordBearer
+from passlib.context import CryptContext
 
 
 load_dotenv(find_dotenv())
@@ -13,3 +15,6 @@ DATABASE_URL: str = os.getenv(
 HASH_SCHEMES: typing.List[str] = ["bcrypt"]
 SECRET_KEY: str = os.getenv("SECRET_KEY", "foo_key_for_testing_purpose")
 TOKEN_URL: str = os.getenv("TOKEN_URL", "token")
+
+OAUTH2_SCHEMA = OAuth2PasswordBearer(tokenUrl=TOKEN_URL)
+PASSWORD_CTXT = CryptContext(schemes=HASH_SCHEMES, deprecated="auto")
