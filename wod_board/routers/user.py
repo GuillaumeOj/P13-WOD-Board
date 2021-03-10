991cd98bfaedc505626848ba48bceb6444699e9f
Guillaume <7831777+GuillaumeOj@users.noreply.github.com>
2021-03-10 23:46:51 +0100

    feat(user): add authentication for user
    Add a route to allow an user to authenticate.
    This route return a Token object with:
    - The access_token
    - The token_type

--- wod_board/routers/user.py
+++ wod_board/routers/user.py
@@ -1,26 +1,26 @@
-import typing
-
 from fastapi import APIRouter
 from fastapi import Depends
+from fastapi import status
 from fastapi.exceptions import HTTPException
 from fastapi.security import OAuth2PasswordRequestForm
 import sqlalchemy.orm
 
+from wod_board import config
 from wod_board.crud import user as user_crud
 from wod_board.models import get_db
-from wod_board.models import user as user_models
 from wod_board.schemas import user as user_schemas
+from wod_board.utils import user as user_utils
 
 
 router = APIRouter(prefix="/user", tags=["user"])
 
 
-@router.post("/register")
+@router.post("/register", response_model=user_schemas.User)
 async def register(
     user: user_schemas.UserCreate, db: sqlalchemy.orm.Session = Depends(get_db)
-) -> user_models.User:
+) -> user_schemas.User:
     try:
-        return user_crud.create_user(db, user)
+        new_user = user_crud.create_user(db, user)
     except user_crud.DuplicatedEmail:
         raise HTTPException(status_code=400, detail="Email already registered")
     except user_crud.DuplicatedUsername:
@@ -30,9 +30,29 @@ async def register(
             status_code=400, detail="Unexcpected error, please try again"
         )
 
+    return user_schemas.User(
+        id=new_user.id,
+        email=new_user.email,
+        username=new_user.username,
+        first_name=new_user.first_name,
+        last_name=new_user.last_name,
+    )
 
-@router.post("/login")
+
+@router.post(f"/{config.ACCESS_TOKEN_URL}", response_model=user_schemas.Token)
 async def login(
     form_data: OAuth2PasswordRequestForm = Depends(),
-) -> typing.Dict[str, str]:
-    pass
+    db: sqlalchemy.orm.Session = Depends(get_db),
+) -> user_schemas.Token:
+    user = user_utils.authenticate_user(db, form_data.username, form_data.password)
+
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect email or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+
+    access_token = user_utils.create_access_token(data={"sub": user.email})
+
+    return user_schemas.Token(access_token=access_token, token_type="bearer")
