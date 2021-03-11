b7ea4a65d4da8e09f012472683be11cd29cdad85
Guillaume <7831777+GuillaumeOj@users.noreply.github.com>
2021-03-11 10:37:15 +0100

    refactor(config): move PASSWORD_CTXT and OAUTH2_SCHEME to utils
    Move PASSWORD_CTXT and OAUTH2_SCHEME from config to wod_board.utils.user
    since it is not really configuration's constant.

--- wod_board/config.py
+++ wod_board/config.py
@@ -3,8 +3,6 @@ import typing
 
 from dotenv import find_dotenv
 from dotenv import load_dotenv
-from fastapi.security.oauth2 import OAuth2PasswordBearer
-from passlib.context import CryptContext
 
 
 load_dotenv(find_dotenv())
@@ -17,6 +15,3 @@ ACCESS_TOKEN_ALGORITHM: str = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES: int = 1440
 ACCESS_TOKEN_URL: str = "login"
 SECRET_KEY: str = os.getenv("SECRET_KEY", "foo_key_for_testing_purpose")
-
-PASSWORD_CTXT = CryptContext(schemes=HASH_SCHEMES, deprecated="auto")
-OAUTH2_SCHEME = OAuth2PasswordBearer(tokenUrl=ACCESS_TOKEN_URL)
