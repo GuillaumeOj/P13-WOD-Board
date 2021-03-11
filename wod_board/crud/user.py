3588774ba12fe077ad776e896e0889a1c7492cb8
Guillaume <7831777+GuillaumeOj@users.noreply.github.com>
2021-03-11 19:10:54 +0100

    refactor(routers): refactor user's router
    I misunderstand the way `response_model` works in routers decorators so
    each endpoint return the same type as the one defined in `response_model`.
    So I had to convert each ORM result in FastAPI's schema.
    
    This is why this refactor endpoints to make them return an ORM model instead
    of a FastAPI's schema.

--- wod_board/crud/user.py
+++ wod_board/crud/user.py
@@ -3,7 +3,7 @@ import typing
 import sqlalchemy.exc
 import sqlalchemy.orm
 
-from wod_board.models import user
+from wod_board.models import user as user_models
 from wod_board.schemas import user as user_schemas
 from wod_board.utils import user as user_utils
 
@@ -16,22 +16,26 @@ class DuplicatedUsername(Exception):
     pass
 
 
-def get_user(db: sqlalchemy.orm.Session, user_id: int) -> typing.Optional[user.User]:
-    return db.query(user.User).filter(user.User.id == user_id).first()
+def get_user(
+    db: sqlalchemy.orm.Session, user_id: int
+) -> typing.Optional[user_models.User]:
+    return db.query(user_models.User).filter(user_models.User.id == user_id).first()
 
 
 def get_user_by_email(
     db: sqlalchemy.orm.Session, user_email: str
-) -> typing.Optional[user.User]:
-    return db.query(user.User).filter(user.User.email == user_email).first()
+) -> typing.Optional[user_models.User]:
+    return (
+        db.query(user_models.User).filter(user_models.User.email == user_email).first()
+    )
 
 
 def create_user(
     db: sqlalchemy.orm.Session, new_user: user_schemas.UserCreate
-) -> user.User:
+) -> user_models.User:
     hashed_password = user_utils.PASSWORD_CTXT.hash(new_user.password)
 
-    db_user = user.User(
+    user = user_models.User(
         email=new_user.email,
         hashed_password=hashed_password,
         username=new_user.username,
@@ -39,7 +43,7 @@ def create_user(
         last_name=new_user.last_name,
     )
 
-    db.add(db_user)
+    db.add(user)
     try:
         db.commit()
     except sqlalchemy.exc.IntegrityError as error:
@@ -55,6 +59,6 @@ def create_user(
         ) in str(error):
             raise DuplicatedUsername
     else:
-        db.refresh(db_user)
+        db.refresh(user)
 
-    return db_user
+    return user
