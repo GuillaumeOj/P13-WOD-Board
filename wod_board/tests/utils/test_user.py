9431d2845fff7d0c920ff9cd271dc17d080222a5
Guillaume <7831777+GuillaumeOj@users.noreply.github.com>
2021-03-12 19:24:12 +0100

    refactor(tests): add tests and refactor get_user_with_token

--- wod_board/tests/utils/test_user.py
+++ wod_board/tests/utils/test_user.py
@@ -11,6 +11,38 @@ def test_create_access_token():
     assert isinstance(token, user_schemas.Token)
 
 
+def test_get_user_with_token(db):
+    user = user_models.User(
+        email="foo@bar.com", username="foo_bar", hashed_password="hashed-password"
+    )
+
+    db.add(user)
+    db.commit()
+
+    token = user_utils.create_access_token(user)
+
+    user_with_token = user_utils.get_user_with_token(db, token)
+    assert isinstance(user_with_token, user_models.User)
+    assert user_with_token.email == user.email
+    assert user_with_token.username == user.username
+
+    user_2 = user_models.User(
+        email=None, username="foo_bar", hashed_password="hashed-password"
+    )
+    token = user_utils.create_access_token(user_2)
+
+    user_with_token = user_utils.get_user_with_token(db, token)
+    assert user_with_token is None
+
+    user_3 = user_models.User(
+        email="foo2@bar.com", username="foo_bar", hashed_password="hashed-password"
+    )
+    token = user_utils.create_access_token(user_3)
+
+    user_with_token = user_utils.get_user_with_token(db, token)
+    assert user_with_token is None
+
+
 def test_authenticate_user(db):
     password = "strong-password"
     hashed_password = user_utils.PASSWORD_CTXT.hash(password)
