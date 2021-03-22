c9658ceacad7205a3a33fde67b7940769c9a9e6d
Guillaume <7831777+GuillaumeOj@users.noreply.github.com>
2021-03-22 10:56:40 +0100

    refactor(routers): refactor routers to avoid same module's name
    Add a prefix to router's module name

--- wod_board/web.py
+++ wod_board/web.py
@@ -1,10 +1,10 @@
 from fastapi import FastAPI
 
-from wod_board.routers import user
-from wod_board.routers import wod
+from wod_board.routers import user_routers
+from wod_board.routers import wod_routers
 
 
 app = FastAPI()
-app.include_router(user.router_user)
-app.include_router(user.router_token)
-app.include_router(wod.router)
+app.include_router(user_routers.router_user)
+app.include_router(user_routers.router_token)
+app.include_router(wod_routers.router)
