4bf92d702cde1d4233471a8d17fb50f29aa5986c
Guillaume <7831777+GuillaumeOj@users.noreply.github.com>
2021-03-22 18:57:40 +0100

    refactor(wod): remove relationship with WodType

--- wod_board/models/wod.py
+++ wod_board/models/wod.py
@@ -13,14 +13,13 @@ class Wod(models.Base):
     date = sqlalchemy.Column(
         sqlalchemy.DateTime, nullable=False, server_default=sqlalchemy.func.now()
     )
-    rounds = sqlalchemy.orm.relationship("Round", cascade="all, delete")
-
     wod_type_id = sqlalchemy.Column(
         sqlalchemy.Integer,
         sqlalchemy.ForeignKey("wod_type.id"),
         nullable=False,
     )
-    wod_type = sqlalchemy.orm.relationship("WodType", back_populates="wods")
+
+    rounds = sqlalchemy.orm.relationship("Round", cascade="all, delete")
 
 
 class WodType(models.Base):
@@ -28,8 +27,6 @@ class WodType(models.Base):
 
     id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True)
     name = sqlalchemy.Column(sqlalchemy.String(150), nullable=False)
-
-    wods = sqlalchemy.orm.relationship("Wod", back_populates="wod_type")
 
 
 class Round(models.Base):
