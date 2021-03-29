a16a09d22d07a874c5918506761e090e39cddae6
Guillaume <7831777+GuillaumeOj@users.noreply.github.com>
2021-03-29 21:14:27 +0200

    feat(register): design register's form

--- src/Register.jsx
+++ src/Register.jsx
@@ -9,7 +9,28 @@ function Register() {
           <p className="lead">Join our community for free!</p>
         </div>
         <div className="subContent">
-          <p>Join our community!</p>
+          <form method="post" action="api/register">
+            <div className="field">
+              <label htmlFor="username">Username*:&nbsp;</label>
+              <input type="text" name="username" id="username" required />
+            </div>
+            <div className="group">
+              <div className="field">
+                <label htmlFor="first_name">First Name:&nbsp;</label>
+                <input type="text" name="first_name" id="first_name" />
+              </div>
+              <div className="field">
+                <label htmlFor="last_name">Last Name:&nbsp;</label>
+                <input type="text" name="last_name" id="last_name" />
+              </div>
+            </div>
+            <div className="field">
+              <label htmlFor="password">Password*:&nbsp;</label>
+              <input type="password" name="password" id="password" required />
+            </div>
+            <p>All fields marked with * are required.</p>
+            <input type="button" value="Register" className="button primary" />
+          </form>
         </div>
       </section>
     </>
