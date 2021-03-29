a865d39daabaeb38dfa970f21cf39cc05d20e5ec
Guillaume <7831777+GuillaumeOj@users.noreply.github.com>
2021-03-29 12:43:36 +0200

    feat(register): add a front component for the register's form

---
+++ src/Register.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function Register() {
+  return (
+    <>
+      <section id="register">
+        <div className="subHeader">
+          <h2 className="title">Join Us!</h2>
+          <p className="lead">Join our community for free!</p>
+        </div>
+        <div className="subContent">
+          <p>Join our community!</p>
+        </div>
+      </section>
+    </>
+  );
+}
+
+export default Register;
