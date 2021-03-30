2722e1c5ea001e80fb4b2b30214ccafeccc498e4
Guillaume <7831777+GuillaumeOj@users.noreply.github.com>
2021-03-30 09:56:42 +0200

    feat(register): handle form submit

--- src/Register.jsx
+++ src/Register.jsx
@@ -1,39 +1,74 @@
 import React from 'react';
 
+import useInput from './Utils';
+
 function Register() {
+  const [username, setUsername] = useInput('');
+  const [password, setPassword] = useInput('');
+  const [firstName, setFirstName] = useInput('');
+  const [lastName, setLastName] = useInput('');
+
+  function handleSubmit(event) {
+    event.preventDefault();
+  }
+
   return (
-    <>
-      <section id="register">
-        <div className="subHeader">
-          <h2 className="title">Join Us!</h2>
-          <p className="lead">Join our community for free!</p>
-        </div>
-        <div className="subContent">
-          <form method="post" action="api/register">
+    <section id="register">
+      <div className="subHeader">
+        <h2 className="title">Join Us!</h2>
+        <p className="lead">Join our community for free!</p>
+      </div>
+      <div className="subContent">
+        <form onSubmit={handleSubmit}>
+          <div className="field">
+            <label htmlFor="username">Username*:&nbsp;</label>
+            <input
+              type="text"
+              name="username"
+              id="username"
+              value={username}
+              onChange={setUsername}
+              required
+            />
+          </div>
+          <div className="group">
             <div className="field">
-              <label htmlFor="username">Username*:&nbsp;</label>
-              <input type="text" name="username" id="username" required />
-            </div>
-            <div className="group">
-              <div className="field">
-                <label htmlFor="first_name">First Name:&nbsp;</label>
-                <input type="text" name="first_name" id="first_name" />
-              </div>
-              <div className="field">
-                <label htmlFor="last_name">Last Name:&nbsp;</label>
-                <input type="text" name="last_name" id="last_name" />
-              </div>
+              <label htmlFor="firstName">First Name:&nbsp;</label>
+              <input
+                type="text"
+                name="firstName"
+                id="firstName"
+                value={firstName}
+                onChange={setFirstName}
+              />
             </div>
             <div className="field">
-              <label htmlFor="password">Password*:&nbsp;</label>
-              <input type="password" name="password" id="password" required />
+              <label htmlFor="lastName">Last Name:&nbsp;</label>
+              <input
+                type="text"
+                name="lastName"
+                id="lastName"
+                value={lastName}
+                onChange={setLastName}
+              />
             </div>
-            <p>All fields marked with * are required.</p>
-            <input type="button" value="Register" className="button primary" />
-          </form>
-        </div>
-      </section>
-    </>
+          </div>
+          <div className="field">
+            <label htmlFor="password">Password*:&nbsp;</label>
+            <input
+              type="password"
+              name="password"
+              id="password"
+              value={password}
+              onChange={setPassword}
+              required
+            />
+          </div>
+          <p>All fields marked with * are required.</p>
+          <input type="submit" value="Register" className="button primary" />
+        </form>
+      </div>
+    </section>
   );
 }
 
