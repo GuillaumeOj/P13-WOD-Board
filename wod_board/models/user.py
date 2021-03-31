ffbf2b2043b4a7c79796d2e71e8761796287cba4
Guillaume <7831777+GuillaumeOj@users.noreply.github.com>
2021-03-31 23:39:55 +0200

    fix(user): add not nullable for required fields

--- wod_board/models/user.py
+++ wod_board/models/user.py
@@ -8,9 +8,9 @@ class User(models.Base):
     __tablename__ = "user"
 
     id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True)
-    email = sqlalchemy.Column(sqlalchemy.String, unique=True)
+    email = sqlalchemy.Column(sqlalchemy.String, unique=True, nullable=False)
     hashed_password = sqlalchemy.Column(sqlalchemy.String, nullable=False)
-    username = sqlalchemy.Column(sqlalchemy.String, unique=True)
+    username = sqlalchemy.Column(sqlalchemy.String, unique=True, nullable=False)
     first_name = sqlalchemy.Column(sqlalchemy.String)
     last_name = sqlalchemy.Column(sqlalchemy.String)
 
