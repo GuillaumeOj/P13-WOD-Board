2b49dc26442463a6db0cb2ef1a33c1883301d090
Guillaume <7831777+GuillaumeOj@users.noreply.github.com>
2021-03-31 23:39:55 +0200

    fix(user): add not nullable for required fields

--- wod_board/tests/models/test_user.py
+++ wod_board/tests/models/test_user.py
@@ -1,3 +1,6 @@
+import pytest
+import sqlalchemy.exc
+
 from wod_board.models import user
 
 
@@ -16,3 +19,56 @@ def test_user(db):
     db.refresh(new_user)
 
     assert str(new_user) == f"<User {new_user.email}>"
+
+
+def test_user_with_required_fields(db):
+    new_user = user.User(
+        hashed_password="foo-password",
+        username="foo-username",
+        first_name="foo",
+        last_name="bar",
+    )
+    db.add(new_user)
+
+    with pytest.raises(sqlalchemy.exc.IntegrityError) as error:
+        db.commit()
+
+    db.rollback()
+    assert (
+        'null value in column "email" of relation "user" '
+        "violates not-null constraint" in str(error)
+    )
+
+    new_user = user.User(
+        email="foo@bar.com",
+        username="foo-username",
+        first_name="foo",
+        last_name="bar",
+    )
+    db.add(new_user)
+
+    with pytest.raises(sqlalchemy.exc.IntegrityError) as error:
+        db.commit()
+
+    db.rollback()
+    assert (
+        'null value in column "hashed_password" of relation "user" '
+        "violates not-null constraint" in str(error)
+    )
+
+    new_user = user.User(
+        email="foo@bar.com",
+        hashed_password="foo-password",
+        first_name="foo",
+        last_name="bar",
+    )
+    db.add(new_user)
+
+    with pytest.raises(sqlalchemy.exc.IntegrityError) as error:
+        db.commit()
+
+    db.rollback()
+    assert (
+        'null value in column "username" of relation "user" '
+        "violates not-null constraint" in str(error)
+    )
