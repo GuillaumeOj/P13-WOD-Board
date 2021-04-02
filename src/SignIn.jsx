23e21a7dd7e6acb0ea424b3c077cf82f72447037
Guillaume <7831777+GuillaumeOj@users.noreply.github.com>
2021-04-02 16:06:59 +0200

    feat(signin): add a form for signing in

---
+++ src/SignIn.jsx
@@ -0,0 +1,81 @@
+import axios from 'axios';
+import React, { useState } from 'react';
+import { NavLink } from 'react-router-dom';
+
+import { useInput, Alert } from './Utils';
+
+function SignIn() {
+  const [email, setEmail] = useInput('');
+  const [password, setPassword] = useInput('');
+
+  const [alert, setAlert] = useState({ content: '', type: '' });
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+
+    const formData = new FormData();
+    formData.append('username', email);
+    formData.append('password', password);
+
+    axios
+      .post('/api/user/token', formData)
+      .then(() => {
+        setAlert({ content: 'You are now logged in!', type: 'success' });
+      })
+      .catch((error) => {
+        if (error.response) {
+          setAlert({ content: error.response.data.detail, type: 'error' });
+        } else if (error.message) {
+          setAlert({ content: error.message, type: 'error' });
+        } else {
+          setAlert({
+            content: 'An error occured, please try again or contact an administrator.',
+            type: 'error',
+          });
+        }
+      });
+  }
+
+  return (
+    <section id="signIn">
+      <div className="subHeader">
+        <h2 className="title">Sign In!</h2>
+      </div>
+      <div className="subContent">
+        <form onSubmit={handleSubmit}>
+          <Alert message={alert.content} type={alert.type} />
+          <div className="field">
+            <label htmlFor="username">Email:&nbsp;</label>
+            {/* The field is named username
+            because we use the 'email' as an 'username' */}
+            <input
+              type="text"
+              name="username"
+              id="username"
+              value={email}
+              onChange={setEmail}
+              required
+            />
+          </div>
+          <div className="field">
+            <label htmlFor="password">Password:&nbsp;</label>
+            <input
+              type="password"
+              name="password"
+              id="password"
+              value={password}
+              onChange={setPassword}
+              required
+            />
+          </div>
+          <input type="submit" value="Sign In" className="button primary" />
+          <p>
+            Don&apos;t have any account? <NavLink to="/register">Register</NavLink> now.
+          </p>
+        </form>
+      </div>
+    </section>
+  );
+}
+
+export default SignIn;
