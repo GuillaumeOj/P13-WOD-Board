92cb3f6ff1b2a487910c229c5bc4b64ba105157a
Guillaume <7831777+GuillaumeOj@users.noreply.github.com>
2021-04-03 15:58:29 +0200

    feat(sentry): add sentry for FastAPI

--- wod_board/web.py
+++ wod_board/web.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+import sentry_sdk
+from sentry_sdk.integrations.asgi import SentryAsgiMiddleware
 
 from wod_board.routers import user_routers
 from wod_board.routers import wod_routers
@@ -8,3 +10,9 @@ app = FastAPI()
 app.include_router(user_routers.router_user)
 app.include_router(user_routers.router_token)
 app.include_router(wod_routers.router)
+
+sentry_sdk.init(
+    dsn="https://9ab3f6551b3549c993c01dcb041bb41d@o453278.ingest.sentry.io/5704632",
+    traces_sample_rate=1.0,
+)
+app.add_middleware(SentryAsgiMiddleware)
