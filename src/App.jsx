758f62580aba82490aea65c097e0dafbff88ac23
Guillaume <7831777+GuillaumeOj@users.noreply.github.com>
2021-04-03 14:49:57 +0200

    refacto(nav): move Navbar in a specific component

--- src/App.jsx
+++ src/App.jsx
@@ -1,56 +1,16 @@
 import React from 'react';
-import { Helmet } from 'react-helmet';
-import { NavLink, Route, Switch } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 
-import { useAuth } from './Auth';
 import Home from './Home';
+import NavBar from './Nav';
 import Register from './Register';
 import SignIn from './SignIn';
 import { NotFound } from './Utils';
 
 function App() {
-  const auth = useAuth();
   return (
     <div className="App">
-      <Helmet>
-        <title>Welcome to WOD Board!</title>
-      </Helmet>
-      <header>
-        <div className="logo">
-          <NavLink exact to="/">
-            <img src={`${process.env.PUBLIC_URL}logo.svg`} className="brand_logo" alt="logo" />
-            <h1 className="brand_name">WOD Board</h1>
-          </NavLink>
-        </div>
-        <nav className="navbar">
-          <ol className="crumbs">
-            <li>
-              <NavLink exact to="/">
-                Home
-              </NavLink>
-            </li>
-            {auth.user ? (
-              <>
-                <li>
-                  <NavLink to="/profile">My Account</NavLink>
-                </li>
-                <li>
-                  <input type="button" onClick={() => auth.signOut()} value="Signout" />
-                </li>
-              </>
-            ) : (
-              <>
-                <li>
-                  <NavLink to="/register">Register</NavLink>
-                </li>
-                <li>
-                  <NavLink to="/signin">Signin</NavLink>
-                </li>
-              </>
-            )}
-          </ol>
-        </nav>
-      </header>
+      <NavBar />
       <div id="content">
         <Switch>
           <Route exact path="/">
