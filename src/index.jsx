6f5bffd7b3aec905ce4c3aa7380e18a55df040d4
Guillaume <7831777+GuillaumeOj@users.noreply.github.com>
2021-04-03 16:01:11 +0200

    feat(sentry): add Sentry to the React application

--- src/index.jsx
+++ src/index.jsx
@@ -1,3 +1,5 @@
+import * as Sentry from '@sentry/react';
+import { Integrations } from '@sentry/tracing';
 import React from 'react';
 import { CookiesProvider } from 'react-cookie';
 import ReactDOM from 'react-dom';
@@ -7,6 +9,16 @@ import App from './App';
 import { ProvideAuth } from './Auth';
 
 import './scss/index.scss';
+
+Sentry.init({
+  dsn: 'https://cff175f8ab4f41e7a1f4f2a35b3e9d2a@o453278.ingest.sentry.io/5704650',
+  integrations: [new Integrations.BrowserTracing()],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+});
 
 ReactDOM.render(
   <React.StrictMode>
