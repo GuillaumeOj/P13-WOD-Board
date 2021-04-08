555970e7ec5fcb1b04fe26f3d2f7aeec2088d3f4
Guillaume <7831777+GuillaumeOj@users.noreply.github.com>
2021-04-08 11:15:20 +0200

    refactor(wod): remove rounds when adding a wod
    To simplify the way we create a wod in the database, we no longer add
    the rounds with crud used by wod

--- wod_board/routers/wod_routers.py
+++ wod_board/routers/wod_routers.py
@@ -1,6 +1,6 @@
-from fastapi import APIRouter
-from fastapi import Depends
+import fastapi
 from fastapi.exceptions import HTTPException
+import sqlalchemy.exc
 import sqlalchemy.orm
 
 from wod_board import config
@@ -9,16 +9,20 @@ from wod_board.models import get_db
 from wod_board.schemas import wod_schemas
 
 
-router = APIRouter(prefix=f"{config.API_URL}/wod", tags=["wod"])
+router = fastapi.APIRouter(prefix=f"{config.API_URL}/wod", tags=["wod"])
 
 
 @router.post("/add", response_model=wod_schemas.Wod)
-async def add(
-    wod_data: wod_schemas.WodCreate, db: sqlalchemy.orm.Session = Depends(get_db)
+async def add_wod(
+    wod_data: wod_schemas.WodCreate,
+    db: sqlalchemy.orm.Session = fastapi.Depends(get_db),
 ) -> wod_schemas.Wod:
     try:
         new_wod = wod_crud.create_wod(db, wod_data)
-    except wod_crud.DuplicatedRoundPosition:
-        raise HTTPException(status_code=400, detail="Rounds have the same position")
+    except sqlalchemy.exc.IntegrityError:
+        raise HTTPException(
+            status_code=fastapi.status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="Something went wrong. Please contact the site administrator.",
+        )
 
     return new_wod
