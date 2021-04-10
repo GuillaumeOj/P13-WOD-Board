32b88dbe90439e7dbdf7662f891cb91769aff18d
Guillaume <7831777+GuillaumeOj@users.noreply.github.com>
2021-04-10 14:15:09 +0200

    refactor: add equipments to movement

--- wod_board/tests/crud/test_movement.py
+++ wod_board/tests/crud/test_movement.py
@@ -1,7 +1,9 @@
 import pytest
 
 from wod_board.crud import movement_crud
+from wod_board.models import equipment
 from wod_board.models import movement
+from wod_board.schemas import equipment_schemas
 from wod_board.schemas import movement_schemas
 
 
@@ -30,10 +32,22 @@ def test_get_movement_by_exact_name(db):
 
 
 def test_get_or_create_movement(db):
-    movement_schema = movement_schemas.MovementCreate(name=MOVEMENT_NAME)
+    dumbbel_schema = equipment_schemas.EquipmentCreate(name="Dumbbel")
+    barbell_schema = equipment_schemas.EquipmentCreate(name="Barbell")
+    equipments = [dumbbel_schema, barbell_schema]
+
+    movement_schema = movement_schemas.MovementCreate(
+        name=MOVEMENT_NAME, equipments=equipments
+    )
 
     wanted_movement = movement_crud.get_or_create_movement(db, movement_schema)
     assert wanted_movement.name == movement_schema.name
+
+    db_equipments = db.query(equipment.Equipment).all()
+    assert wanted_movement.equipments == [
+        equipment_schemas.Equipment.from_orm(db_equipment)
+        for db_equipment in db_equipments
+    ]
 
     wanted_movement = movement_crud.get_or_create_movement(db, movement_schema)
     assert wanted_movement.name == movement_schema.name
