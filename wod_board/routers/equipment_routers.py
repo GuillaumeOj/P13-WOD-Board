8459f7e49d30c6b75b91938803edfc75f361ddc9
Guillaume <7831777+GuillaumeOj@users.noreply.github.com>
2021-04-10 19:13:11 +0200

    feat: add router for equipment

---
+++ wod_board/routers/equipment_routers.py
@@ -0,0 +1,38 @@
+import fastapi
+from fastapi import status
+from fastapi.exceptions import HTTPException
+import sqlalchemy.orm
+
+from wod_board import config
+from wod_board.crud import equipment_crud
+from wod_board.models import get_db
+from wod_board.schemas import equipment_schemas
+
+
+router = fastapi.APIRouter(prefix=f"{config.API_URL}/equipment", tags=["equipment"])
+
+
+@router.post("/", response_model=equipment_schemas.Equipment)
+async def add_equipment(
+    equipment_data: equipment_schemas.EquipmentCreate,
+    db: sqlalchemy.orm.Session = fastapi.Depends(get_db),
+) -> equipment_schemas.Equipment:
+    return equipment_schemas.Equipment.from_orm(
+        equipment_crud.get_or_create_equipment(db, equipment_data)
+    )
+
+
+@router.get("/{name}")
+async def get_equipment_by_exact_name(
+    name: str,
+    db: sqlalchemy.orm.Session = fastapi.Depends(get_db),
+) -> equipment_schemas.Equipment:
+    try:
+        return equipment_schemas.Equipment.from_orm(
+            equipment_crud.get_equipment_by_exact_name(db, name)
+        )
+    except equipment_crud.UnknownEquipment:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"{name} doesn't exist yet",
+        )
---
+++ wod_board/tests/routers/test_equipment.py
@@ -0,0 +1,44 @@
+import pytest
+
+from wod_board.models import equipment
+
+
+@pytest.mark.asyncio
+async def test_add(db, client):
+    equipment_json = {"name": "Dumbbel", "unit_id": None}
+    response = await client.post("/api/equipment/", json=equipment_json)
+
+    expected_response = {
+        "id": 1,
+        "name": "Dumbbel",
+        "unit_id": None,
+    }
+
+    assert response.status_code == 200
+    assert response.json() == expected_response
+
+
+@pytest.mark.asyncio
+async def test_get_unit_by_exact_name(db, client):
+    name = "Dumbbel"
+    response = await client.get(f"/api/equipment/{name}")
+
+    expected_response = {"detail": f"{name} doesn't exist yet"}
+
+    assert response.status_code == 404
+    assert response.json() == expected_response
+
+    wanted_equipment = equipment.Equipment(name=name, unit_id=None)
+    db.add(wanted_equipment)
+    db.commit()
+
+    response = await client.get(f"/api/equipment/{name}")
+
+    expected_response = {
+        "id": 1,
+        "name": "Dumbbel",
+        "unit_id": None,
+    }
+
+    assert response.status_code == 200
+    assert response.json() == expected_response
