027e86547aeebef9fa3532f8d1e5af80e6680a3f
Guillaume <7831777+GuillaumeOj@users.noreply.github.com>
2021-04-11 12:54:32 +0200

    feat: add router and crud for getting a wod by id

--- wod_board/routers/wod_routers.py
+++ wod_board/routers/wod_routers.py
@@ -1,4 +1,6 @@
 import fastapi
+from fastapi import status
+from fastapi.exceptions import HTTPException
 import sqlalchemy.orm
 
 from wod_board import config
@@ -16,3 +18,17 @@ async def add_wod(
     db: sqlalchemy.orm.Session = fastapi.Depends(get_db),
 ) -> wod_schemas.Wod:
     return wod_schemas.Wod.from_orm(wod_crud.create_wod(db, wod_data))
+
+
+@router.get("/{id}", response_model=wod_schemas.Wod)
+async def get_wod_by_id(
+    id: int,
+    db: sqlalchemy.orm.Session = fastapi.Depends(get_db),
+) -> wod_schemas.Wod:
+    try:
+        return wod_schemas.Wod.from_orm(wod_crud.get_wod_by_id(db, id))
+    except wod_crud.UnknownWod:
+        raise HTTPException(
+            status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+            detail="This WOD doesn't exist",
+        )
