8ad2b8aba382cb190d698855b264c64feefac8c0
Guillaume <7831777+GuillaumeOj@users.noreply.github.com>
2021-04-13 20:41:01 +0200

    refactor: refactor round (schemas, crud and models)

--- wod_board/crud/round_crud.py
+++ wod_board/crud/round_crud.py
@@ -1,10 +1,15 @@
 import typing
 
+import daiquiri
 import sqlalchemy.exc
 import sqlalchemy.orm
 
+from wod_board.crud import movement_crud
 from wod_board.models import wod_round
 from wod_board.schemas import round_schemas
+
+
+LOG = daiquiri.getLogger(__name__)
 
 
 class DuplicatedRoundPosition(Exception):
@@ -34,14 +39,20 @@ def get_round_by_id(
 def create_round(
     db: sqlalchemy.orm.Session,
     round_data: round_schemas.RoundCreate,
-    parent_round_id: typing.Optional[int] = None,
+    parent_id: typing.Optional[int] = None,
 ) -> wod_round.Round:
     new_round = wod_round.Round(
         position=round_data.position,
         duration_seconds=round_data.duration_seconds,
         wod_id=round_data.wod_id,
-        parent_round_id=parent_round_id,
+        parent_id=parent_id,
     )
+
+    if round_data.movements:
+        new_round.movements = [
+            movement_crud.create_movement_goal(db, move)
+            for move in round_data.movements
+        ]
 
     db.add(new_round)
 
@@ -65,8 +76,7 @@ def create_round(
 
     if round_data.sub_rounds:
         for sub_round in round_data.sub_rounds:
-            create_round(db, sub_round, parent_round_id=new_round.id)
-
-    db.refresh(new_round)
+            create_round(db, sub_round, parent_id=new_round.id)
+        db.refresh(new_round)
 
     return new_round
--- wod_board/tests/crud/test_round.py
+++ wod_board/tests/crud/test_round.py
@@ -1,27 +1,27 @@
 import pytest
 
 from wod_board.crud import round_crud
+from wod_board.models import movement
 from wod_board.models import wod
 from wod_board.models import wod_round
+from wod_board.schemas import movement_schemas
 from wod_board.schemas import round_schemas
 
 
 def test_create_round(db):
-    wod_type = wod.WodType("AMRAP")
-    new_wod = wod.Wod(wod_type_id=1)
-    db.add(wod_type)
-    db.add(new_wod)
+    wod_type = wod.WodType(name="AMRAP")
+    db_wod = wod.Wod(wod_type=wod_type)
+    db.add(db_wod)
     db.commit()
-    db.refresh(new_wod)
+    db.refresh(db_wod)
 
-    first_round = round_schemas.RoundCreate(position=1, wod_id=new_wod.id)
-    first_sub_round = round_schemas.RoundCreate(position=2, wod_id=new_wod.id)
-    first_round.sub_rounds = [first_sub_round]
+    sub_round = round_schemas.RoundCreate(position=2, wod_id=db_wod.id)
+    first_round = round_schemas.RoundCreate(
+        position=1, wod_id=db_wod.id, sub_rounds=[sub_round]
+    )
 
     round_crud.create_round(db, first_round)
-
-    db_rounds = db.query(wod_round.Round).all()
-    assert len(db_rounds) == 2
+    assert db.query(wod_round.Round).count() == 2
 
     parent_round = (
         db.query(wod_round.Round).filter(wod_round.Round.position == 1).first()
@@ -29,16 +29,36 @@ def test_create_round(db):
     assert parent_round.sub_rounds.count() == 1
 
 
-def test_create_rounds_with_duplicated_position(db):
-    wod_type = wod.WodType("AMRAP")
-    new_wod = wod.Wod(wod_type_id=1)
-    db.add(wod_type)
-    db.add(new_wod)
+def test_create_round_with_mouvement(db):
+    wod_type = wod.WodType(name="AMRAP")
+    db_wod = wod.Wod(wod_type=wod_type)
+    db.add(db_wod)
     db.commit()
-    db.refresh(new_wod)
+    db.refresh(db_wod)
 
-    first_round = round_schemas.RoundCreate(position=1, wod_id=new_wod.id)
-    first_sub_round = round_schemas.RoundCreate(position=1, wod_id=new_wod.id)
+    devil_press = movement_schemas.MovementCreate(name="Devil Press")
+    db.add(movement.Movement(name=devil_press.name))
+    db.commit()
+
+    goal = movement_schemas.MovementGoalCreate(movement=devil_press)
+
+    first_round = round_schemas.RoundCreate(
+        position=1, wod_id=db_wod.id, movements=[goal]
+    )
+
+    round_crud.create_round(db, first_round)
+    assert db.query(movement.MovementGoal).count() == 1
+
+
+def test_create_rounds_with_duplicated_position(db):
+    wod_type = wod.WodType(name="AMRAP")
+    db_wod = wod.Wod(wod_type=wod_type)
+    db.add(db_wod)
+    db.commit()
+    db.refresh(db_wod)
+
+    first_round = round_schemas.RoundCreate(position=1, wod_id=db_wod.id)
+    first_sub_round = round_schemas.RoundCreate(position=1, wod_id=db_wod.id)
     first_round.sub_rounds = [first_sub_round]
 
     with pytest.raises(round_crud.DuplicatedRoundPosition):
