54019cd23d7d01c273c7cef6d7d3570f3fcbb8d9
Guillaume <7831777+GuillaumeOj@users.noreply.github.com>
2021-04-27 20:43:18 +0200

    feat: add movement to a round

--- frontend/src/Dashboard/WodComposer/Round.jsx
+++ frontend/src/Dashboard/WodComposer/Round.jsx
@@ -1,8 +1,12 @@
 import PropTypes from 'prop-types';
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
+
+import { v4 as uuidV4 } from 'uuid';
 
 import { RoundPropType } from '../../Type';
 import { useInput } from '../../Utils';
+
+import Movement from './Movement';
 
 export default function Round({ round, removeRound, updateRound }) {
   const { position, id } = round;
@@ -10,6 +14,50 @@ export default function Round({ round, removeRound, updateRound }) {
   const [repetition, setRepetition] = useInput(round.repetition);
   const [durationMinutes, setDurationMinutes] = useInput(round.durationMinutes);
   const [durationSeconds, setDurationSeconds] = useInput(round.durationSeconds);
+
+  const [movements, setMovements] = useState([]);
+
+  const removeMovement = (movementId) => {
+    const updatedMovements = movements.filter((item) => item.id !== movementId);
+    setMovements(updatedMovements);
+  };
+
+  const addMovement = () => {
+    const newMovements = [...movements];
+    newMovements.push({
+      id: uuidV4(),
+      movementId: null,
+      roundId: null,
+      name: '',
+      repetition: 0,
+      durationMinutes: 0,
+      durationSeconds: 0,
+    });
+
+    setMovements(newMovements);
+  };
+
+  const updateMovement = (movement) => {
+    const updatedMovements = [...movements];
+
+    if (movement) {
+      setMovements(
+        updatedMovements.map((item) => {
+          if (item.id === movement.id) {
+            return {
+              ...item,
+              name: movement.name,
+              movementId: movement.movementId,
+              repetition: movement.repetition,
+              durationMinutes: movement.durationMinutes,
+              durationSeconds: movement.durationSeconds,
+            };
+          }
+          return item;
+        }),
+      );
+    }
+  };
 
   useEffect(() => {
     updateRound({
@@ -64,6 +112,18 @@ export default function Round({ round, removeRound, updateRound }) {
             />
           </div>
         </div>
+        <button className="button primary" type="button" onClick={addMovement}>
+          Movement +
+        </button>
+        {movements
+          && movements.map((movement) => (
+            <Movement
+              key={movement.id}
+              movement={movement}
+              removeMovement={removeMovement}
+              updateMovement={updateMovement}
+            />
+          ))}
       </div>
     )
   );
