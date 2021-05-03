ca8a2c9b6aac23dd3a86401d4d31f80de99050a1
Guillaume <7831777+GuillaumeOj@users.noreply.github.com>
2021-05-03 17:47:42 +0200

    refactor(wod): add authentication on router for creating a WOD

--- backend/wod_board/routers/wod_routers.py
+++ backend/wod_board/routers/wod_routers.py
@@ -18,6 +18,14 @@ from wod_board.utils import user_utils
 router = fastapi.APIRouter(prefix=f"{config.API_URL}/wod", tags=["wod"])
 
 
+class AuthorNotUser(HTTPException):
+    def __init__(self):
+        super().__init__(
+            status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+            detail="Author don't match with authenticated user",
+        )
+
+
 @router.get(
     "/types/{name}", response_model=typing.List[typing.Optional[wod_schemas.WodType]]
 )
@@ -34,6 +42,9 @@ async def create_wod(
     db: sqlalchemy.orm.Session = fastapi.Depends(get_db),
     current_user: user.User = fastapi.Depends(user_utils.get_user_with_token),
 ) -> wod.Wod:
+    if wod_data.author_id != current_user.id:
+        raise AuthorNotUser
+
     try:
         return wod_crud.create_wod(db, wod_data)
     except wod_crud.UnknownWodType:
