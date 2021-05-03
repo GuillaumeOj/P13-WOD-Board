11a85a31793a6d8501f8d65d4354d9f1239e3f78
Guillaume <7831777+GuillaumeOj@users.noreply.github.com>
2021-05-03 19:08:53 +0200

    refactor(unit): add admin authentication on router for creating a unit

--- backend/wod_board/tests/routers/test_unit.py
+++ backend/wod_board/tests/routers/test_unit.py
@@ -4,18 +4,37 @@ from wod_board.models import unit
 
 
 @pytest.mark.asyncio
-async def test_add(db, client):
-    unit_json = {"name": "Kilograms", "symbol": "kg"}
-    response = await client.post("/api/unit/", json=unit_json)
+async def test_create_unit(db, client, token, token_admin):
+    assert db.query(unit.Unit).count() == 0
 
+    unit_json = {"name": "Kilograms", "symbol": "kg"}
+    response = await client.post(
+        "/api/unit/",
+        json=unit_json,
+        headers={"Authorization": f"Bearer {token_admin.access_token}"},
+    )
     expected_response = {
         "id": 1,
         "name": "Kilograms",
         "symbol": "kg",
     }
-
     assert response.status_code == 200
     assert response.json() == expected_response
+    assert db.query(unit.Unit).count() == 1
+
+    response = await client.post("/api/unit/", json=unit_json)
+    assert response.status_code == 401
+    assert response.json() == {"detail": "Not authenticated"}
+    assert db.query(unit.Unit).count() == 1
+
+    response = await client.post(
+        "/api/unit/",
+        json=unit_json,
+        headers={"Authorization": f"Bearer {token.access_token}"},
+    )
+    assert response.status_code == 401
+    assert response.json() == {"detail": "Need admin rights"}
+    assert db.query(unit.Unit).count() == 1
 
 
 @pytest.mark.asyncio
