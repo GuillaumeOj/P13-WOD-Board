be79ecfec8ebeb4b0fd9bd0657c6b6fdcd09642f
Guillaume <7831777+GuillaumeOj@users.noreply.github.com>
2021-05-03 19:08:53 +0200

    refactor(unit): add admin authentication on router for creating a unit

--- backend/wod_board/routers/unit_routers.py
+++ backend/wod_board/routers/unit_routers.py
@@ -6,18 +6,25 @@ import sqlalchemy.orm
 from wod_board import config
 from wod_board.crud import unit_crud
 from wod_board.models import get_db
+from wod_board.models import unit
+from wod_board.models import user
 from wod_board.schemas import unit_schemas
+from wod_board.utils import user_utils
 
 
 router = fastapi.APIRouter(prefix=f"{config.API_URL}/unit", tags=["unit"])
 
 
 @router.post("/", response_model=unit_schemas.Unit)
-async def add_unit(
+async def create_unit(
     unit_data: unit_schemas.UnitCreate,
     db: sqlalchemy.orm.Session = fastapi.Depends(get_db),
-) -> unit_schemas.Unit:
-    return unit_schemas.Unit.from_orm(unit_crud.get_or_create_unit(db, unit_data))
+    current_user: user.User = fastapi.Depends(user_utils.get_user_with_token),
+) -> unit.Unit:
+    if not current_user.is_admin:
+        raise user_utils.IsNotAdmin
+
+    return unit_crud.get_or_create_unit(db, unit_data)
 
 
 @router.get("/{name}")
