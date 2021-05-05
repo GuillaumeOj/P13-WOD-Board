c1faccc7201287858701e8196cb130bf50c48d62
Guillaume <7831777+GuillaumeOj@users.noreply.github.com>
2021-05-05 18:55:11 +0200

    refactor: move exceptions raised by routers in specific module

--- backend/wod_board/routers/wod_routers.py
+++ backend/wod_board/routers/wod_routers.py
@@ -7,6 +7,7 @@ import sqlalchemy.orm
 
 from wod_board import config
 from wod_board import exceptions
+from wod_board import exceptions_routers
 from wod_board.crud import wod_crud
 from wod_board.models import get_db
 from wod_board.models import user
@@ -16,14 +17,6 @@ from wod_board.utils import user_utils
 
 
 router = fastapi.APIRouter(prefix=f"{config.API_URL}/wod", tags=["wod"])
-
-
-class AuthorNotUser(HTTPException):
-    def __init__(self):
-        super().__init__(
-            status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
-            detail="Author don't match with authenticated user",
-        )
 
 
 @router.get(
@@ -43,7 +36,7 @@ async def create_wod(
     current_user: user.User = fastapi.Depends(user_utils.get_user_with_token),
 ) -> wod.Wod:
     if wod_data.author_id != current_user.id:
-        raise AuthorNotUser
+        raise exceptions_routers.AuthorNotUser
 
     try:
         return wod_crud.create_wod(db, wod_data)
@@ -67,7 +60,7 @@ async def update_wod(
     current_user: user.User = fastapi.Depends(user_utils.get_user_with_token),
 ) -> wod.Wod:
     if wod_data.author_id != current_user.id:
-        raise AuthorNotUser
+        raise exceptions_routers.AuthorNotUser
 
     try:
         return wod_crud.update_wod(db, wod_data, id)
