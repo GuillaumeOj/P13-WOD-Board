aab7e5ffa3604f36a76cb5aeb2de6693779f120e
Guillaume <7831777+GuillaumeOj@users.noreply.github.com>
2021-05-10 16:32:32 +0200

    fix(auth): fix cookies
    Cookies are created with useEffect on component mount and updated with
    another useEffet.
    If the user is set to null the cookie is removed

--- frontend/src/Auth.jsx
+++ frontend/src/Auth.jsx
@@ -1,5 +1,6 @@
+import PropTypes from 'prop-types';
 import React, {
-  useEffect, useState, useContext, createContext,
+  createContext, useEffect, useState, useContext,
 } from 'react';
 import { useCookies } from 'react-cookie';
 import { useHistory } from 'react-router-dom';
@@ -11,25 +12,31 @@ const authContext = createContext();
 function useProvideAuth() {
   const history = useHistory();
   const { addAlert } = useAlert();
-  const [cookies, setCookie, removeCookie] = useCookies(['user']);
+  const [cookies, setCookies, removeCookies] = useCookies();
   const [user, setUser] = useState(null);
 
   const signOut = () => {
-    removeCookie('user');
     setUser(null);
-    addAlert({ message: 'You are logged out.', alertType: 'success' });
-    history.push('/');
+    addAlert({ message: 'You are logged out.', alertType: 'warning' });
+    history.replace('/');
   };
 
   const signIn = (tokenData) => {
-    if (tokenData) {
-      setCookie('user', tokenData, { path: '/' });
-    }
+    setUser(tokenData);
+    addAlert({ message: 'You are logged in.', alertType: 'success' });
   };
 
   useEffect(() => {
     setUser(cookies.user);
-  }, [cookies]);
+  }, []);
+
+  useEffect(() => {
+    if (user) {
+      setCookies('user', user, { path: '/' });
+    } else {
+      removeCookies('user');
+    }
+  }, [user]);
 
   return {
     user,
@@ -38,10 +45,12 @@ function useProvideAuth() {
   };
 }
 
-// eslint-disable-next-line react/prop-types
 export function ProvideAuth({ children }) {
   const auth = useProvideAuth();
   return <authContext.Provider value={auth}>{children}</authContext.Provider>;
 }
+ProvideAuth.propTypes = {
+  children: PropTypes.element.isRequired,
+};
 
 export const useAuth = () => useContext(authContext);
