4fd6cf449f2a20f79b5dae2fb39572d6adac16b3
Guillaume <7831777+GuillaumeOj@users.noreply.github.com>
2021-06-12 23:42:39 +0200

    chore: remove Sentry in the React app

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,5 +1,3 @@
-import * as Sentry from '@sentry/react';
-import { Integrations } from '@sentry/tracing';
 import React from 'react';
 import { CookiesProvider } from 'react-cookie';
 import ReactDOM from 'react-dom';
@@ -11,19 +9,6 @@ import App from './App';
 import { ProvideAuth } from './Auth';
 
 import './scss/index.scss';
-
-if (process.env.NODE_ENV === 'production') {
-  Sentry.init({
-    dsn:
-      'https://cff175f8ab4f41e7a1f4f2a35b3e9d2a@o453278.ingest.sentry.io/5704650',
-    integrations: [new Integrations.BrowserTracing()],
-
-    // Set tracesSampleRate to 1.0 to capture 100%
-    // of transactions for performance monitoring.
-    // We recommend adjusting this value in production
-    tracesSampleRate: 1.0,
-  });
-}
 
 ReactDOM.render(
   <React.StrictMode>
